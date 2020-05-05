11d5c1cd2c703938de5eac96bc72075f70cbaaf8
TymonKilich <tymon.kilich@pg.edu.pl>
2020-05-05 04:04:41 +0200

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,6 @@ using mopkg
 using Test
 
 @testset "mopkg.jl" begin
-    @test example_fun(2) == 4
-    @test example_fun(-2) == 4
+    @test example_func(2) == 4
+    @test example_func(-2) == 4
 end
