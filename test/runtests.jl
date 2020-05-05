f2dccf0a4c0fbe684b91317ae489e1cc5df3a800
TymonKilich <tymon.kilich@pg.edu.pl>
2020-05-05 04:10:18 +0200

    Add tests for second function

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,13 @@
 using mopkg
 using Test
 
-@testset "mopkg.jl" begin
+@testset "Function1" begin
     @test example_func(2) == 4
     @test example_func(-2) == 4
+end
+
+@testset "Function2" begin
+    @test example_func2(2) == 0
+    @test example_func2(-3) == -5
+    @test example_func2(9) == 7
 end
