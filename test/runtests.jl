b4f671b49b622b3a4d03fab9e20474957db56ba5
konto-testowe-mo20 <64876999+konto-testowe-mo20@users.noreply.github.com>
2020-05-07 02:25:24 +0200

    Add additional test function

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,8 @@ Single variable global test functions – functions with single global minimum
 key is function (lambda), value (y, x) in minimum
 """
 svltf = Dict(
-    (x -> x^2 - 1) => (-1.0, 0.0))
+    (x -> x^2 - 1) => (-1.0, 0.0),
+    (x -> x^2/3 + 2x - sin(x) => (-3.424528818, -3.99083))
 
 @testset "Single variable optimizers" begin
     @testset "General test for SVOptMethods" begin
